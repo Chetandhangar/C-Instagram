9c2ed40c7ad2860c83a92f77f6e7aa75eb93f480
Chetan Dhangar <chetansdhangar@gmail.com>
2021-01-13 23:02:54 +0530

    add- search users using firebase

--- components/Main/Search.js
+++ components/Main/Search.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React,{useState} from 'react';
+import {View, Text, TextInput, FlatList} from 'react-native';
+import firebase from 'firebase';
+require('firebase/firestore');
+require('firebase/firebase-storage');
+
 
 export default function Search(){
+    
+    const [users, setUsers] = useState([])
+    
+
+    const fetchUsers = (search) => { 
+        firebase.firestore()
+        .collection('users')
+        .where('name', '>=' , search )
+        .get()
+        .then((snapshot) =>{
+            let users = snapshot.docs.map(doc =>{
+                const data = doc.data();
+                console.log(data);
+                const id = doc.id;
+                return{id , ...data}
+            })
+            console.log(users)
+            setUsers(users);
+        })
+    }
+
+
     return(
         <View>
             <Text>
                 Search Component
             </Text>
+
+            <TextInput 
+            style={{marginTop:15}}
+            placeholder="Search User..."
+            onChangeText={(search) => fetchUsers(search)}
+            />
+            
+            <FlatList 
+            data={users}
+            numColumns={1}
+            horizontal={false}
+            renderItem={({item})=>(
+            <Text>{item.name}</Text>
+            )}
+            />
+           
         </View>
     );
 }
